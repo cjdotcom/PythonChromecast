169d64db556e84e5eaf0d53cb387b105eacfa1f8
unknown <gabriel.silva-h@hotmail.com>
2023-04-17 13:55:20 -0300

    Primeiro commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import pychromecast
+import time
+
+chromecasts, browser = pychromecast.get_listed_chromecasts(friendly_names=["Gabriel TV"])
+  
+try:
+    cast = chromecasts[0]
+    cast.wait()
+    mc = cast.media_controller
+    mc.play_media('https://img.ibxk.com.br/materias/5866/21577.jpg', 'image/jpeg')
+    mc.block_until_active()
+    mc.play()
+    time.sleep(20)
+    mc.stop()
+except:
+    print("Nenhum Chromecast encontrado na rede.")
